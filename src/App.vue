fc2c0013489cebfb8f3fcd5774f9a06eb2ab7ae4
shad0w <kirillpashnin.dev@gmail.com>
2025-10-13 20:52:43 +0200

    Added main menu logic

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
-  <GameEngine />
+  <MainMenu v-if="!startGame" @startGame="startGame = true" />
+  <GameEngine v-else-if="startGame" />
 </template>
 <script setup>
+import { ref } from "vue";
 import GameEngine from "./components/GameEngine.vue";
+import MainMenu from "./components/MainMenu.vue";
+const startGame = ref(false);
 </script>
 <style scoped></style>
---
+++ src/components/MainMenu.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Main menu</h1>
+  <button @click="emit('startGame', true)">Start the game!</button>
+</template>
+<script setup>
+const emit = defineEmits(["startGame"]);
+</script>
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+h1 {
+  font-size: 3em;
+}
+
+button {
+  padding: 10px 20px;
+  font-size: 3em;
+}
+</style>
